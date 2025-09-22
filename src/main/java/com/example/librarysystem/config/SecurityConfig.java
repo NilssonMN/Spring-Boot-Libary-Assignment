ffde747212ca72a9a5ae5412c1c080962884cfc8
nilsson <mansnilsson2001@hotmail.com>
2025-09-23 01:43:14 +0200

    1234

--- src/main/java/com/example/librarysystem/config/SecurityConfig.java
+++ src/main/java/com/example/librarysystem/config/SecurityConfig.java
@@ -1,5 +1,6 @@
 package com.example.librarysystem.config;
 
+import com.example.librarysystem.Handler.CustomAccessDeniedHandler;
 import com.example.librarysystem.service.UserDetailServiceImpl;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -19,29 +20,38 @@ public class SecurityConfig {
     private final UserDetailServiceImpl userDetailsService;
     private final AuthenticationSuccessHandler successHandler;
     private final AuthenticationFailureHandler failureHandler;
+    private final CustomAccessDeniedHandler accessDeniedHandler;
 
     public SecurityConfig(UserDetailServiceImpl userDetailsService,
                           AuthenticationSuccessHandler successHandler,
-                          AuthenticationFailureHandler failureHandler) {
+                          AuthenticationFailureHandler failureHandler,
+                          CustomAccessDeniedHandler accessDeniedHandler) {
         this.userDetailsService = userDetailsService;
         this.successHandler = successHandler;
         this.failureHandler = failureHandler;
+        this.accessDeniedHandler = accessDeniedHandler;
     }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.authorizeHttpRequests(auth ->
-                        auth.requestMatchers("/books/**").hasAnyRole("ADMIN", "USER")
+                                auth.requestMatchers("/login").permitAll()
                                 .requestMatchers("/authors/**").hasRole("ADMIN")
+                                .requestMatchers("/users/*/loans/**").hasRole("ADMIN")
                                 .requestMatchers("/users/**").hasRole("ADMIN")
                                 .requestMatchers("/loans/**").hasRole("ADMIN")
                                 .anyRequest().authenticated()
                 )
-                .httpBasic(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults()) // Enable Basic Auth for Postman
                 .formLogin(form -> form
+                        .loginPage("/login")
                         .successHandler(successHandler)
                         .failureHandler(failureHandler)
                         .permitAll()
+                )
+                .exceptionHandling(exceptions -> exceptions
+                        .accessDeniedHandler(accessDeniedHandler)
+                        .authenticationEntryPoint(accessDeniedHandler)
                 )
                 .userDetailsService(userDetailsService)
                 .logout(logout -> logout
@@ -50,6 +60,7 @@ public class SecurityConfig {
                         .permitAll()
                 );
 
+        http.csrf(csrf -> csrf.disable()); // Disable CSRF for Postman testing
         return http.build();
     }
 
